07193028db3309bc9f45dad70292aaf0adb44bfa
vochuong57 <vochuong57@gmail.com>
2024-11-11 17:29:22 +0700

    16-Socials-sharing-&-Wishlist

--- woocommerce_theme/woocommerce.php
+++ woocommerce_theme/woocommerce.php
@@ -11,7 +11,24 @@
     <?php woocommerce_content(); ?>
 </div>
 
-
 <?php 
     get_footer();
 ?>
+
+<script type="text/javascript">
+    jQuery(document).ready(function($) {
+        // Kiểm tra nếu footer nằm trong .container
+        if ($('footer').closest('.container').length) {
+            // Di chuyển footer ra ngoài container
+            $('footer').appendTo('body');
+        }
+        
+        $('ul').each(function() {
+            // Kiểm tra nếu trong <ul> có thẻ <p>
+            if ($(this).find('p').length > 0) {
+                // Xóa thẻ <p>
+                $(this).find('p').remove();
+            }
+        });
+    });
+</script>
